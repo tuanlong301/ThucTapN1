84acf052a5f7b4bb18dd9b9cfc35f4e4762a24d7
tuanlong301 <goituan36@gmail.com>
2025-08-15 08:27:23 +0700

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    alias(libs.plugins.android.application) apply false
+}
+
+buildscript {
+
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2") // Sử dụng cú pháp Kotlin
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,30 @@
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "appbanhang"
+include(":app")
